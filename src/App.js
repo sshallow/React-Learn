d064e7598324dae404ddaf224d8303a362ba332a
shangshy <doit_2017@outlook.com>
2023-09-10 20:03:26 +0800

    module 4

--- src/App.js
+++ src/App.js
@@ -1,64 +1,32 @@
 import './App.css';
 import {User} from './User';
 import {Planet} from "./Planet";
+import {useState} from "react";
 
 function App() {
 
-    // const age = 12;
-    // const isGreen = true
-    //
-    // return (
-    //     <div className="App"> {age >= 18 ? <h1> overage </h1> : <h1>underage </h1>}
-    //     <h1 style={{color: "red"}}> This a color</h1>
-    //         {isGreen && <h1> This is green</h1>}
-    //     </div>
-    // )
+    // const [age, setAge] = useState(21)
+    const [inputValue, setInputValue] = useState("")
 
-    // const names = ["尚帅", "张三", "李四", "王五", "赵六"]
-    //
-    // const users = [
-    //     { name: "尚帅", age: 12, email: "123@qq.com" },
-    //     { name: "张三", age: 12, email: "123@qq.com" },
-    //     { name: "李四", age: 12, email: "123@qq.com" },
-    //     { name: "王五", age: 12, email: "123@qq.com" },
-    // ]
-    //
-    // return (
-    //     <div className="App">
-    //         {
-    //             users.map((user, index) => {
-    //                 console.log(user, index);
-    //                 return <User key={index} name={user.name} age={user.age} email={user.email} />
-    //             })
-    //         }
-    //     </div>
-    // )
+    // const increaseAge = () => {
+    //     setAge(age - 1)
+    // }
 
-    const planets = [
-        {name: "Mercury", isGasPlanet: true},
-        {name: "Venus", isGasPlanet: true},
-        {name: "Earth", isGasPlanet: true},
-        {name: "Mars", isGasPlanet: false},
-        {name: "Jupiter", isGasPlanet: true},
-        {name: "Saturn", isGasPlanet: false},
-    ]
+    const onChange = (e) => {
+        setInputValue(e.target.value)
+    }
 
     return (
         <div className="App">
-            {
-                planets.map((planet, index) => planet.isGasPlanet && <Planet key={index} name={planet.name} isGasPlanet={planet.isGasPlanet} />)
-            }
+           {/* //{age}*/}
+           {/* //<button onClick={increaseAge}> Increase Age</button>*/}
+           <input type="text" onChange={onChange}/>
+            <h1>
+
+                {inputValue}
+            </h1>
         </div>
     )
-}
-
-
-const GetName = () => {
-    return "尚帅";
-}
-
-const GetNameComponent = () => {
-    return <h1>{GetName()}</h1>;
 }
 
 export default App;
