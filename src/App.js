5297d69d9de3d8c3469f3b63459b9ac95582efeb
shangshy <doit_2017@outlook.com>
2023-09-10 18:03:09 +0800

    module 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import './App.css';
+import {User} from './User';
+import  {Planet} from "./Planet";
 
 function App() {
 
-    return (<div className="App">
-            <h1 className="name">尚帅</h1>
-            <User className="name" name="尚帅" age={30} email="doit_2017@outlook.com"/>
-            <User name="张三" age={20} email="doit_2017@outlook.com"/>
-            <User name="李四" age={10} email="doit_2017@outlook.com"/>
-        </div>
+    // const age = 12;
+    // const isGreen = true
+    //
+    // return (
+    //     <div className="App"> {age >= 18 ? <h1> overage </h1> : <h1>underage </h1>}
+    //     <h1 style={{color: "red"}}> This a color</h1>
+    //         {isGreen && <h1> This is green</h1>}
+    //     </div>
+    // )
 
-    );
-}
+    // const names = ["尚帅", "张三", "李四", "王五", "赵六"]
+    //
+    // const users = [
+    //     { name: "尚帅", age: 12, email: "123@qq.com" },
+    //     { name: "张三", age: 12, email: "123@qq.com" },
+    //     { name: "李四", age: 12, email: "123@qq.com" },
+    //     { name: "王五", age: 12, email: "123@qq.com" },
+    // ]
+    //
+    // return (
+    //     <div className="App">
+    //         {
+    //             users.map((user, index) => {
+    //                 console.log(user, index);
+    //                 return <User key={index} name={user.name} age={user.age} email={user.email} />
+    //             })
+    //         }
+    //     </div>
+    // )
 
-const User = (props) => {
+    const planets = [
+        { name: "Mercury", isGasPlanet: true },
+        { name: "Venus", isGasPlanet: true },
+        { name: "Earth", isGasPlanet: true },
+        { name: "Mars", isGasPlanet: false },
+        { name: "Jupiter", isGasPlanet: true },
+        { name: "Saturn", isGasPlanet: false },
+    ]
+
     return (
-        <div>
-            <h1>{props.name}</h1>
-            <h1>{props.age}</h1>
-            <h1>{props.email}</h1>
+        <div className="App">
+            {
+                planets.map((planet, index) => {
+                    return <Planet key={index} name={planet.name} isGasPlanet={planet.isGasPlanet} />
+                })
+            }
         </div>
     )
 }
+
+
 
 const GetName = () => {
     return "尚帅";
---
+++ src/Planet.jsx
@@ -0,0 +1,9 @@
+export const Planet = (props) => {
+    return (
+        props.isGasPlanet && (
+            <h1>
+                {props.name}
+            </h1>
+        )
+    )
+}
