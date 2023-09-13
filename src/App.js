3ab0001cbe528534d921a42563b0798042554109
shangshy <doit_2017@outlook.com>
2023-09-13 23:04:08 +0800

    module 7

--- src/App.js
+++ src/App.js
@@ -4,26 +4,46 @@ import {Planet} from "./Planet";
 import {useEffect, useState} from "react";
 import {Task} from "./Task";
 import {Text} from "./Text";
+import Axios from "axios";
+import axios from "axios";
 
 function App() {
 
-    const [showText, setShowText] = useState(false)
+    // const [catFact, setCatFact] = useState("");
+    //
+    // const fetchCatFact = () => {
+    //     Axios.get('https://catfact.ninja/fact').then((res) => {
+    //         console.log(res.data)
+    //         setCatFact(res.data.fact)
+    //     })
+    // }
+    // useEffect(() => {
+    //     Axios.get('https://catfact.ninja/fact')
+    //         .then((res) => {
+    //                 console.log(res.data)
+    //                 setCatFact(res.data.fact)
+    //             }
+    //         )
+    // }, [])
 
-
-
-
-    const [text, setText] = useState("")
-
-    const inputOnChange = (e) => {
-        setText(e.target.value)
+    const [age, setAge] = useState({});
+    const [name, setName] = useState({});
+    const predictAge = () => {
+        axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+            console.log(res.data)
+            setAge(res.data)
+        })
     }
-    return (
-        <div className="App">
-            <button onClick={() => setShowText(!showText)}> show text</button>
-            {showText && (
-                <Text inputOnChange={inputOnChange} text={text}/>
-            )}
-
+    return (<div className="App">
+            {/*<h1 className="title">React</h1>*/}
+            {/*<button onClick={fetchCatFact}> get cat fact </button>*/}
+            {/*<h3>*/}
+            {/*    {catFact}*/}
+            {/*</h3>*/}
+            <input type="text" placeholder="Enter your name" onChange={(e) => setName(e.target.value)}/>
+            <button onClick={predictAge}>预测年龄</button>
+            <h1>{age?.age}岁</h1>
+            <h1>{age?.count}个</h1>
         </div>
 
     )
